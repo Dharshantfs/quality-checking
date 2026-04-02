c2f68c02d45f6989a89ed06d795eb6088e916cdd
Dharshantfs <dharsofficialacc@gmail.com>
2026-04-02 15:33:37 +0530

    Match GSM table layout to Excel screenshot and enforce read-only permanence.

--- quality_gsm_app/install.py
+++ quality_gsm_app/install.py
@@ -7,12 +7,14 @@ def _run_quality_checking_setup():
     from quality_gsm_app.patches.v2_0 import create_quality_checking_only
     from quality_gsm_app.patches.v1_3 import force_quality_checking_parent_fix
     from quality_gsm_app.patches.v1_4 import ensure_quality_checking_permissions
-    from quality_gsm_app.patches.v2_1 import update_gsm_report_layout
+    from quality_gsm_app.patches.v2_2 import update_gsm_report_layout_excel_style
+    from quality_gsm_app.patches.v2_3 import enforce_field_permanence
 
     create_quality_checking_only.execute()
     force_quality_checking_parent_fix.execute()
     ensure_quality_checking_permissions.execute()
-    update_gsm_report_layout.execute()
+    update_gsm_report_layout_excel_style.execute()
+    enforce_field_permanence.execute()
     frappe.db.commit()
 
 
